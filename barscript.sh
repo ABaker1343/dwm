deca532c027cdccc4ca75ab5693a3c355fcffb4b
ABaker1343 <billybo1234@outlook.com>
2022-05-25 19:52:05 +0100

    updated barscript

--- barscript.sh
+++ barscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 status () {
-    echo -n "Battery: $(acpi | awk '{print $4}' | sed -n '2p') Vol: $(pactl get-sink-volume 0 | awk '{print $5}') $(date)"
+    echo -n "Battery: $(acpi | awk '{print $4}' | sed -n '2p') | Vol: $(pactl get-sink-volume 0 | awk '{print $5}') | $(date)"
 }
 
 while true
