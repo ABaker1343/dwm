e0fcea755439cbee94cae0b907d504f1a065d8e4
ABaker1343 <billybo1234@outlook.com>
2022-05-26 17:42:36 +0100

    display script on all screens

--- barscript.sh
+++ barscript.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 status () {
-    echo -n "Battery: $(acpi | awk '{print $4}' | sed -n '2p') | Vol: $(pactl get-sink-volume 0 | awk '{print $5}') | $(date)"
+    MEMTOTAL=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
+    MEMAVAILABLE=$(cat /proc/meminfo | grep MemAvailable | awk '{print $2}')
+    echo -n "Battery: $(acpi | awk '{print $4}' | sed -n '2p') | Vol: $(pactl get-sink-volume 0 | awk '{print $5}') | Mem: $(( ($MEMTOTAL - $MEMAVAILABLE)/1000 ))MB | $(date)"
 }
-
 while true
 do
 xsetroot -name "$(status)"
