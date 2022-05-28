07d147a6fea44f9430374278b5803125d556b561
ABaker1343 <billybo1234@outlook.com>
2022-05-28 22:33:36 +0100

    updated barscript to be more universal

--- barscript.sh
+++ barscript.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
 status () {
-    #MEMTOTAL=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
-    #MEMAVAILABLE=$(cat /proc/meminfo | grep MemAvailable | awk '{print $2}')
-    #MEMTOTAL=$(free | grep Mem | awk '{print $2}')
-    #MEMFREE=$(free | grep Mem | awk '{print $7}')
-    #MEMUSED=$(free | grep Mem | awk 'print $3')
-    echo -n "Battery: $(acpi | awk '{print $4}' | sed -n '2p') | Vol: $(pactl get-sink-volume 0 | awk '{print $5}') | Mem: $(( $(free | grep Mem | awk '{print $3}')/1024 ))MB | $(date)"
+    echo -n "Battery: $(acpi | awk '{print $4}' | sed -n '2p') | Vol: $(pactl get-sink-volume @DEFAULT_SINK | awk '{print $5}') | Mem: $(( $(free | grep Mem | awk '{print $3}')/1024 ))MB | $(date)"
 }
-while true
-do
-xsetroot -name "$(status)"
-sleep 1
-done
+
+status_without_battery(){
+    echo -n "Vol: $(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}') | Mem: $(( $(free | grep Mem | awk '{print $3}')/1024 ))MB | $(date)"
+}
+
+if ! command -v acpi
+then
+    while true
+    do
+        xsetroot -name "$(status_without_battery)"
+        sleep 1
+    done
+else
+    while true
+    do
+        xsetroot -name "$(status)"
+        sleep 1
+    done
+fi
