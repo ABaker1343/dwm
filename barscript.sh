2b8b5034b37cda3f6ddf847e30e9cfaaad9b268d
ABaker1343 <billybo1234@outlook.com>
2022-05-31 16:16:20 +0100

    updated barscript

--- barscript.sh
+++ barscript.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 status () {
-    echo -n "[Battery: $(acpi | awk '{print $4}' | sed -n '2p')] | [Vol: $(pactl get-sink-volume @DEFAULT_SINK | awk '{print $5}')] | [Mem: $(( $(free | grep Mem | awk '{print $3}')/1024 ))MB] | $(date)"
+    echo -n "[Battery: $(acpi | awk '{print $4}' | sed -n '2p')] | [🔊: $(pactl get-sink-volume @DEFAULT_SINK | awk '{print $5}')] | [Mem: $(( $(free | grep Mem | awk '{print $3}')/1024 ))MB] | [$(date '+%a %b %d %H:%M')]"
 }
 
 status_without_battery(){
-    echo -n "[Vol: $(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}')] | [Mem: $(( $(free | grep Mem | awk '{print $3}')/1024 ))MB] | $(date)"
+    echo -n "[🔊 $(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}')] | [Mem: $(( $(free | grep Mem | awk '{print $3}')/1024 ))MB] | [$(date '+%a %b %d %H:%M')]"
 }
 
 if ! command -v acpi
